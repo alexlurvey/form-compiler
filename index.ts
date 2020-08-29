3117e3546e0597eeaa719aa86b5d9d88502bbd20
alexlurvey <alex.lurvey@gmail.com>
2020-08-29 12:50:56 -0500

    feat: allow specifying arguments via command line flags

--- index.ts
+++ index.ts
@@ -6,28 +6,34 @@ import { buildFileContexts, writeToFile } from './src/file-writers';
 import { program } from './src/parser';
 
 const inputfile = process.argv.length > 2 && process.argv[2];
-
 if (!inputfile) {
-    console.error('Need a TypeScript file as first argument')
+    console.error('Need a TypeScript file as first argument');
+    process.exit(1);
 } else if (!existsSync(inputfile)) {
     console.error(`${inputfile} doesn't exist`);
-} else {
-    const outdir = process.argv.length > 3 ? process.argv[3] : 'build'
-    const [ schemaFilename, extension ] = inputfile.split('.');
-    const schemaPath = __dirname + '/' + schemaFilename + '.' + extension;
-    const buildPath = __dirname + '/' + outdir;
-
-    const schema = readFileSync(schemaPath, 'utf8');
-    const ctx = defContext(schema, { debug: false });
-    program(ctx);
-    const asts: AST[] = buildAst(ctx.result)
-
-    // copy provided TS schema file
-    copyFileSync(schemaPath, buildPath + '/' + schemaFilename + '.' + extension)
-    const formAst = asts.filter((q: AST) => q[0].name === 'Form') // TODO
-    const contexts = formAst.reduce(buildFileContexts(buildPath, schemaFilename), []) as IFileContext[];
-    const indexCtxs = Array.from(new Set(contexts.map(q => q.filepath)))
-        .map(filepath => ({ filepath, filename: FileType.Index, fileType: FileType.Index }));
-
-    [ ...contexts, ...indexCtxs ].forEach(ctx => writeToFile(ctx))
+    process.exit(1);
 }
+
+let tmpIdx;
+const outdir = (tmpIdx = process.argv.indexOf('--outDir'), tmpIdx === -1 ? 'build' : process.argv[tmpIdx+1]);
+const rootFormName = (tmpIdx = process.argv.indexOf('--rootName'), tmpIdx === -1 ? 'Form' : process.argv[tmpIdx+1]);
+
+const [ schemaFilename, extension ] = inputfile.split('.');
+const schemaPath = __dirname + '/' + schemaFilename + '.' + extension;
+const buildPath = __dirname + '/' + outdir;
+
+const schema = readFileSync(schemaPath, 'utf8');
+const ctx = defContext(schema, { debug: false });
+program(ctx);
+const asts: AST[] = buildAst(ctx.result)
+
+// copy provided TS schema file
+copyFileSync(schemaPath, buildPath + '/' + schemaFilename + '.' + extension)
+
+const formAst = asts.filter((q: AST) => q[0].name === rootFormName)
+const contexts = formAst.reduce(buildFileContexts(buildPath, schemaFilename), []) as IFileContext[];
+
+const indexCtxs = Array.from(new Set(contexts.map(q => q.filepath)))
+    .map(filepath => ({ filepath, filename: FileType.Index, fileType: FileType.Index }));
+
+[ ...contexts, ...indexCtxs ].forEach(ctx => writeToFile(ctx))
